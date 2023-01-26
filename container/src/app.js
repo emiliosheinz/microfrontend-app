9a737e1fef39e831e15fe9e6ab86bcdb053c1bd7
Emilio Schaedler Heinzmann <emilio.heinzmann@cloudbeds.com>
2023-01-26 17:34:49 -0300

    chore: removes context h1

--- container/src/app.js
+++ container/src/app.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp'
 
 const App = () => {
   return <>
-    <h1>Container Context</h1>
+    <h1>Container</h1>
     <hr/>
     <MarketingApp />
   </>
