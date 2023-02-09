3cccbd6e328a8b0ee456e2e5787e8f338f246f85
Emilio Schaedler Heinzmann <emilio.heinzmann@cloudbeds.com>
2023-02-09 16:58:59 -0300

    chore(common): :wrench: add output public path to all projects

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -6,6 +6,9 @@ const packageJson = require('../package.json')
 
 const devConfig = {
   mode: 'development',
+  output: {
+    publicPath: 'http://localhost:8080/'
+  },
   devServer: {
     port: 8080,
     historyApiFallback: true,
