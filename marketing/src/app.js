6b0dce6e3898caedd73eb4b08308f06d59a674d9
Emilio Schaedler Heinzmann <emilio.heinzmann@cloudbeds.com>
2023-02-09 16:03:40 -0300

    feat(common): :sparkles: creates navigation handling between container and marketing

--- marketing/src/app.js
+++ marketing/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Router, Route, Switch } from 'react-router-dom'
 
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
@@ -9,17 +9,21 @@ const generateClassName = createGenerateClassName({
   productionPrefix: 'ma'
 })
 
-const App = () => {
-  return <div>
-    <StylesProvider generateClassName={generateClassName}>
-      <BrowserRouter>
-        <Switch>
-          <Route exact path='/pricing' component={Pricing}/>
-          <Route path='/' component={Landing}/>
-        </Switch>
-      </BrowserRouter>
-    </StylesProvider>
-  </div>
+const App = (props) => {
+  const { history } = props
+
+  return (
+    <div>
+      <StylesProvider generateClassName={generateClassName}>
+        <Router history={history}>
+          <Switch>
+            <Route exact path='/pricing' component={Pricing}/>
+            <Route path='/' component={Landing}/>
+          </Switch>
+        </Router>
+      </StylesProvider>
+    </div>
+  )
 }
 
 export default App
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,20 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history'
 
 import App from './app'
 
-const mount = (element) => {
+const mount = (element, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory()
+
+  if(onNavigate) history.listen(onNavigate)
+
   ReactDOM.render(
-    <App/>,
+    <App history={history}/>,
     element
   )
+
+  return {
+    onParentNavigate({ pathname: nextPathname }) {
+      const { pathname } = history.location
+
+      if(pathname !== nextPathname) history.push(nextPathname)
+    }
+  }
 }
 
 if(process.env.NODE_ENV === 'development') {
   const devRootNode = document.querySelector('#marketing-dev-root-node')
 
   if(devRootNode) {
-    mount(devRootNode)
+    mount(devRootNode, { defaultHistory: createBrowserHistory() })
   }
 }
 
